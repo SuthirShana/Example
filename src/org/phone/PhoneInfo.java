1c5480e5c0d5193d78b50abf41e4e89fcd4899e7
SuthirShana <suthirshana96@gmail.com>
2022-07-09 12:16:13 +0530

    git push

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,39 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("Phone Name is One plus");
+
+	}
+	private void phoneIMEI() {
+		System.out.println("Phone IMEI num is 12345678901234");
+
+	}
+	private void phoneCamera() {
+		System.out.println("Phone Camera is 48megapixel");
+
+	}
+	private void phoneStorage() {
+		System.out.println("Phone Storage is 128GB");
+
+	}
+	private void phoneOsName() {
+		System.out.println("Phone Os Name is OXYGEN");
+
+	}
+	public static void main(String[] args) {
+		
+
+	PhoneInfo phone =new PhoneInfo();
+	phone.phoneName();
+	phone.phoneIMEI();
+	phone.phoneCamera();
+	phone.phoneStorage();
+	phone.phoneOsName();
+	
+	
+	}
+	
+	
+
+}
